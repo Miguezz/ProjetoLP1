185978ca454fec3fd6ad428c915dceb43b08a22b
Felipe Brasileiro <felipe.tanajura@gmail.com>
2021-05-06 16:42:33 -0300

    Implementacao inicial das Classes

--- src/projetolp1/Classes/Alquimista.java
+++ src/projetolp1/Classes/Alquimista.java
@@ -4,11 +4,54 @@
  * and open the template in the editor.
  */
 package projetolp1.Classes;
-
+import projetolp1.Principal.Personagem;
+import projetolp1.Misc.Dano;
 /**
  *
  * @author felip
  */
 public abstract class Alquimista extends ClasseMae {
+    Personagem self; // Link para o personagem dono da que possui a classe Alquimista
+    public Alquimista(Personagem self){
+        super(self);
+        this.ranges[0] = 1;
+        this.ranges[1] = 1;
+        this.ranges[2] = 1;
+        this.self = self;
+    }
     
+    // Lembrar de implementar formulas melhores pra as funcoes
+    /**
+     *
+     * @param target Personagem alvo
+     * @return
+     */
+    @Override
+    public boolean habDano(Personagem target){
+        if(target.getPosicaoNoMapa() - this.self.getPosicaoNoMapa() <= this.ranges[0]){
+            float formulaDano = 10 + this.self.getDano().getValor();
+            Dano dano = new Dano(0, formulaDano);
+            target.setDanoRecebido(dano);
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean habDef(Personagem target){
+        if(target.getPosicaoNoMapa() - this.self.getPosicaoNoMapa() <= this.ranges[1]){
+            System.out.println("Defesa");
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean ultimate(Personagem target){
+        if(target.getPosicaoNoMapa() - this.self.getPosicaoNoMapa() <= this.ranges[2]){
+            float formulaDano = 20 + this.self.getDano().getValor(); 
+            Dano dano = new Dano(0, formulaDano);
+            target.setDanoRecebido(dano);
+        }
+        
+        return true;
+    }
 }
