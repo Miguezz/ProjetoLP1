daaa20d49ce484e84b6dad858baf5030bd6c3763
Joao Humberto Braz <joaohbmenezes@outlook.com>
2021-05-11 10:46:05 -0300

    Criacao das 4 armaduras, falta a descricao de manteau e valores para defesa

---
+++ src/projetolp1/Items/Equipamento/ArmaduraMetalica.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolp1.Items.Equipamento;
+
+class ArmaduraMetalica extends Armadura{
+  ArmaduraMetalica(){
+    super("Armadura Metálica", "Armadura resistente, feita de ferro", 1);
+  }
+}
