835281409c55124c70d8cdfa54d811b32812c826
FelipeLeaoDias <felipeleaodias12345@gmail.com>
2021-06-15 00:39:31 -0300

    Inicio loja

--- src/projetolp1/GUI/LojaController.java
+++ src/projetolp1/GUI/LojaController.java
@@ -9,23 +9,48 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 
 /**
  *
  * @author felip
  */
+
+
 public class LojaController extends Stager implements Initializable {
 
     @FXML
+    private Button Voltar, ComprarU1, ComprarU2;
+    
+    @FXML
+    private Label LapaCoinU1, LapaCoinU2, VenderJogador;
+
     private void acaoClick(ActionEvent event) throws Exception {
-    }
-    
-    
-    
-    public void initialize(URL url, ResourceBundle rb){
         
-    }
+        if (event.getSource() == Voltar) {
+            System.out.println("Voltando para o menu");
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = (Parent)loader.load(getClass().getResource("fxml/CriacaoUsuario.fxml").openStream());
+            CriacaoUsuarioController cuc = loader.getController();
+            cuc.setStage(this.getStage());
+            Scene sc = new Scene(root);
+            sc.getStylesheets().addAll(this.getClass().getResource("menuprincipalpane.css").toExternalForm());
+            this.getStage().setScene(sc);
+            this.getStage().show();
+        }else if (event.getSource() == ComprarU1){
+            System.out.println("Usuario 1 comprou 1 item");
+        }else if (event.getSource() == ComprarU2){
+            System.out.println("Usuario 2 comprou 1 item");
+        }
+
     
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }   
 }
