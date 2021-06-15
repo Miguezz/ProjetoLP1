4565d1002ce7f41a601f8c2676cc6073a2d0eaf6
FelipeLeaoDias <felipeleaodias12345@gmail.com>
2021-06-14 21:12:58 -0300

    inicio de loja

---
+++ src/projetolp1/GUI/LojaController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolp1.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author felip
+ */
+public class LojaController extends Stager implements Initializable {
+
+    @FXML
+    private void acaoClick(ActionEvent event) throws Exception {
+    }
+    
+    
+    
+    public void initialize(URL url, ResourceBundle rb){
+        
+    }
+    
+}
